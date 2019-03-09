1867cbde12df1a2108ef9ad95713f236d0a5e7b7
Rawad <rawadhabibeng@gmail.com>
2019-03-08 23:34:47 -0500

    Initial commit

---
+++ app/src/main/java/example/org/test/weekend04sol/Encode.java
@@ -0,0 +1,13 @@
+package example.org.test.weekend04sol;
+
+public class Encode {
+    public String encode (String string){
+        StringBuilder stringBuilder = new StringBuilder();
+        char tmp = string.charAt(0);
+        int count = 1;
+        for (int i=0; i<string.length();i++){
+            char curr = string.charAt(i);
+            //if (char[i]=="a")
+        }
+    }
+}
